d0dcf653eadcbd78bd3b469cec2b799ebd9f4b82
meg63 <mechavarriac@unal.edu.co>
2023-02-25 19:07:22 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -8,4 +8,19 @@ class Control:
         return self._tv;
     def setTV(self,tv):
         self._tv=tv
+    def turnOn(self):
+        self._tv.turnOn()
+    def turnOff(self):
+        self._tv.turnOff()
+    def canalUp(self):
+        self._tv.canalUp()
+    def canalDown(self):
+        self._tv.canalDown()
+    def volumenUp(self):
+        self._tv.volumenUp
+    def volumenDown(self):
+        self._tv.volumenDown()
+    def setCanal(self):
+        self._tv.setCanal()
+    
     
