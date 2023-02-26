d3ea10d2bb24c3c40d4614dd51e24b5c3281d4dd
meg63 <mechavarriac@unal.edu.co>
2023-02-25 19:10:34 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -5,7 +5,7 @@ class TV:
         self._estado=estado
         self._canal=1
         self._precio=500
-        self._volumen=0
+        self._volumen=1
         self._control=''
         TV.numTV+=1
     def setMarca(self,marca):
