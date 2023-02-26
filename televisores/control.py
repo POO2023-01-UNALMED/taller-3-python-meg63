ce87690cbc0c1fce4b24329965c0a1ecb35e4983
meg63 <mechavarriac@unal.edu.co>
2023-02-25 19:22:48 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -20,7 +20,7 @@ class Control:
         self._tv.volumenUp
     def volumenDown(self):
         self._tv.volumenDown()
-    def setCanal(self):
-        self._tv.setCanal()
+    def setCanal(self,canal):
+        self._tv.setCanal(canal)
     
     
